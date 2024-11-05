9731261915d85b420b147668e71f4f74dfbeb3c0
Akshay <jadhavakshay0504@gmail.com>
2024-11-05 13:03:00 +0530

    new pract file added

--- pygame/main.py
+++ pygame/main.py
@@ -1,19 +1,27 @@
+# Example file showing a basic pygame "game loop"
 import pygame
+
+# pygame setup
 pygame.init()
-
-# Set up display
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Pygame Window")
-
-# Main loop
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
 running = True
+
 while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))  # White background
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
     pygame.display.flip()
 
-pygame.quit()
+    clock.tick(60)  # limits FPS to 60
 
+pygame.quit()
